97c71fe05f27be1c078a77f3aab00c4e4a5c4551
Eusha Ibne Nur Sami <saminfs9@gmail.com>
2026-08-22 08:20:30 +0100

    Adding employee data info & Constructor

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,16 @@
-public class Employee {
+public class Employee {     //defining info
+    private int employeeId;
+    private String name;
+    private String department;
+    private String email;
+    private float leaveBalance;
+    public Employee(int employeeId, //adding Constructor
+                    String name,
+                    String department,
+                    String email,
+                    float leaveBalance) {
+
+
+
+    }
 }
