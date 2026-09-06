4fdc38cc3d1f6a3029fad3a54ca22b85248aaa67
Eusha Ibne Nur Sami <saminfs9@gmail.com>
2026-09-06 14:08:58 +0100

    completing V1 full code tested and successfull.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import java.time.LocalDate;
 import java.util.Scanner;
+import java.time.LocalDateTime;
 public class Main {
     public static void main (String[] args){
 
         //Create scanner
         Scanner scanner =new Scanner(System.in);
         Employee employee = null;
-        LeaveRequest leaverequest = null;
+        LeaveRequest leaveRequest = null;
 
         int choice =0 ;
         while(choice != 5) {
@@ -46,16 +48,87 @@ public class Main {
                 );
                 System.out.println();
                 System.out.println("Employee created successfully!");
-            } else if (choice == 2) {
+            }
+            //Submitting Leave Request
+            else if (choice == 2) {
 
-                System.out.print("Submit Leave Request Selected ");
-            } else if (choice == 3) {
-                System.out.print("View Employee Selected ");
-            } else if (choice == 4) {
-                System.out.print("View Leave Request Selected ");
-            } else if (choice == 5) {
-                System.out.print("Exit");
+                System.out.println("Submit Leave Request Selected ");
+                if(employee == null) {
+                    System.out.println("No employee Created Yet, PLease Creat an " +
+                            "employee first");
+                }else{
+                    System.out.print("Enter Request ID");
+                    int requestId = scanner.nextInt();
+
+                    System.out.print("Enter Employee Id");
+                    int enteredEmployeeId = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if(enteredEmployeeId != employee.getEmployeeId()){
+                        System.out.println("Employee ID does not match");
+                    }else{
+                        System.out.print("Enter Start Date(YYYY-MM-DD): ");
+                        String startDateText = scanner.nextLine();
+                        System.out.print(
+                                "Enter End Date (YYYY-MM-DD): "
+                        );
+
+                        String endDateText = scanner.nextLine();
+
+                        LocalDate startDate = LocalDate.parse(startDateText);
+                        LocalDate endDate = LocalDate.parse(endDateText);
+
+
+                        if (endDate.isBefore(startDate)) {
+
+                            System.out.println(
+                                    "End date cannot be before start date."
+                            );
+                        }else{
+                            System.out.println("Enter Leave Type");
+                            String leaveType = scanner.nextLine();
+
+                            System.out.println("Enter Reason");
+                            String reason = scanner.nextLine();
+                            leaveRequest = new LeaveRequest(
+                                    requestId,
+                                    enteredEmployeeId,
+                                    startDate, endDate, leaveType, reason
+                            );
+                            System.out.println();
+                            System.out.println("Leave request submitted successfully");
+
+                        }
+                    }
+
+                }
+            }
+            //View employee
+            else if (choice == 3) {
+                System.out.println();
+                System.out.println("____Employee Details____");
+                if(employee == null){
+                    System.out.println("No employee has been created yet");
+                }else{
+                    employee.displayInfo();
+                }
+            } // view Leave Request
+
+            else if (choice == 4) {
+                System.out.println();
+                System.out.print("---- Leave Request Details ----");
+                if(leaveRequest == null){
+                    System.out.println("No leave request has been Submitted Yet.");
+                }else{
+                    leaveRequest.displayInfo();
+                }
+            } // Exit session
+            else if (choice == 5) {
+                System.out.println();
+                System.out.println("Exiting Leave Tracking System");
+                System.out.print("GoodBye");
             } else {
+                System.out.println();
                 System.out.println("Invalid choice, please choose between 1 to 5");
             }
         }
